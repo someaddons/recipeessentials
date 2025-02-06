657177005bac610f3310602a5e22517b3a171f62
someaddons <someaddons@github.com>
2025-02-06 23:15:11 +0100

    4.0: Shorten recipe error messages, while keeping useful information

--- src/main/java/com/recipeessentials/recipecache/RecipeManager.java
+++ src/main/java/com/recipeessentials/recipecache/RecipeManager.java
@@ -59,7 +59,7 @@ public class RecipeManager extends net.minecraft.world.item.crafting.RecipeManag
     {
         long hash = calcHash(inventoryIn, recipeTypeIn);
         final CachedRecipeList recipes = recipeCache.get(hash);
-        if (recipes != null && recipes.useCount > 10 && RecipeEssentials.rand.nextInt(recipes.useCount) != 0)
+        if (recipes != null && recipes.useCount > 10 && RecipeEssentials.rand.nextInt(recipes.useCount * 30) != 0)
         {
             recipes.useCount++;
 
@@ -96,7 +96,7 @@ public class RecipeManager extends net.minecraft.world.item.crafting.RecipeManag
     {
         long hash = calcHash(inventoryIn, recipeTypeIn);
         final CachedRecipeList recipes = recipeCache.get(hash);
-        if (recipes != null && recipes.useCount > 10 && RecipeEssentials.rand.nextInt(recipes.useCount) != 0)
+        if (recipes != null && recipes.useCount > 10 && RecipeEssentials.rand.nextInt(recipes.useCount * 30) != 0)
         {
             recipes.useCount++;
 
@@ -150,7 +150,7 @@ public class RecipeManager extends net.minecraft.world.item.crafting.RecipeManag
     public <C extends Container, T extends Recipe<C>> List<T> getRecipesFor(RecipeType<T> recipeTypeIn, C inventoryIn, Level worldIn)
     {
         final CachedRecipeList recipes = recipeCache.get(calcHash(inventoryIn, recipeTypeIn));
-        if (recipes != null && recipes.useCount > 10 && RecipeEssentials.rand.nextInt(recipes.useCount) != 0)
+        if (recipes != null && recipes.useCount > 10 && RecipeEssentials.rand.nextInt(recipes.useCount * 30) != 0)
         {
             recipes.useCount++;
             List<T> matches = new ArrayList<>();
